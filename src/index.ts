f90e25e8f4420f03da734cb4530e7b8073b753b1
Akira Shinohara <akira.shinohara@mojamoja.cloud>
2020-12-04 21:32:15 +0900

    feat: init discord bot

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,20 @@
+import Discord from 'discord.js';
+
+const { DISCORD_TOKEN } = process.env;
+const client = new Discord.Client();
+
 function greeting(target: string): void {
   console.log(`Hello ${target}!`);
 }
 
-greeting('World');
+client.on('ready', () => {
+  greeting('World');
+});
+
+client.on('message', (msg) => {
+  if (msg.content === '/ping') {
+    msg.reply('pong!');
+  }
+});
+
+client.login(DISCORD_TOKEN);
