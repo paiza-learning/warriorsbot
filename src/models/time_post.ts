ccc9f2a1efef30aec93b9a6a30e2d0c92ef2a2e7
cwd-k2 <kaito.augmented7th@gmail.com>
2020-12-05 19:36:03 +0900

    元投稿へのリンク追加

--- src/models/time_post.ts
+++ src/models/time_post.ts
@@ -8,26 +8,25 @@ export interface TimePostAuthor {
 
 export default class TimePost {
   author: TimePostAuthor;
-  channel: string;
   text: string;
 
   constructor(msg: Message) {
-    const channelObj = msg.channel as TextChannel;
+    const channel = msg.channel as TextChannel;
 
     this.author = {
       id: msg.author.id,
       username:
-        channelObj.guild.member(msg.author)?.nickname || msg.author.username,
+        channel.guild.member(msg.author)?.nickname || msg.author.username,
       iconURL: msg.author.displayAvatarURL(),
     };
 
-    this.channel = channelObj.name;
-    this.text = msg.content;
+    console.log(msg);
+    this.text = `${msg.content}\n> **[original post](<${msg.url}>)** <#${channel.id}>`;
   }
 
   webhookMessageOptions(): WebhookMessageOptions {
     const webhookMessageOptions = {
-      username: `${this.author.username} #${this.channel}`,
+      username: `${this.author.username} (Impersonator)`,
       avatarURL: this.author.iconURL,
     } as WebhookMessageOptions;
 
