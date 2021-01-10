e35c8d855dcc1e661660656da3082707f582fec5
Akira Shinohara <akira.shinohara@mojamoja.cloud>
2021-01-10 17:10:35 +0900

    feat: mv ping command from simple command

--- src/handler/index.ts
+++ src/handler/index.ts
@@ -5,6 +5,11 @@ import { TimeLineWebhookClient } from '../models/webhookClient';
 export namespace Handler {
   // TODO: 複雑化してきたらnamespaceごとにファイル分割する
 
+  // tutorial: https://paiza-learning.github.io/warriorsbot/#/tutorial
+  export function pingHandler(msg: Message) {
+    msg.reply('pong!');
+  }
+
   /*
    * timesの投稿をフックしてtimelineに集約する
    */
--- src/models/command/simples/index.ts
+++ src/models/command/simples/index.ts
@@ -1,4 +1,3 @@
 // コマンドを作製したらここに並べていくことで登録完了
 import './paizaIO';
-import './ping';
 import './moja';
