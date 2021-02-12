dd94e0d1f4672f8e4c593ebd073d9896a79a5305
cwd-k2 <kaito.augmented7th@gmail.com>
2021-02-12 23:40:01 +0900

    introduce commando

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import Discord from 'discord.js';
 import Debug from 'debug';
+import path from 'path';
+import { CommandoClient } from 'discord.js-commando';
+
 import { Constants } from './constants';
 import { Router } from './routers';
 
@@ -9,10 +12,26 @@ debug('warriors debug mode on.');
 const DiscordConstants = Constants.Discord;
 const client = new Discord.Client();
 
+const commandClient = new CommandoClient({
+  commandPrefix: '/',
+});
+
+commandClient.registry
+  .registerDefaultTypes()
+  .registerGroups([['paiza', 'Commands created by paiza-learning team.']])
+  .registerDefaultGroups()
+  .registerDefaultCommands()
+  .registerCommandsIn(path.join(__dirname, 'commands'));
+
 client.on('ready', () => {
+  debug('ready.');
+});
+
+commandClient.on('ready', () => {
   debug('ready.');
 });
 
 client.on('message', Router.MessageRouter);
 
 client.login(DiscordConstants.BOT_TOKEN);
+commandClient.login(DiscordConstants.BOT_TOKEN);
