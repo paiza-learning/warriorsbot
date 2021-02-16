6f82a2b7ee97dd0ce030c2af32e526c4aa7b5359
Akira Shinohara <akira.shinohara@mojamoja.cloud>
2021-02-17 01:13:25 +0900

    feat: create todo_* commands

---
+++ src/commands/paiza/todo_add.ts
@@ -0,0 +1,45 @@
+import {
+  Command,
+  CommandInfo,
+  CommandoClient,
+  CommandoMessage,
+} from 'discord.js-commando';
+import { Todo } from '../../entities/todo';
+
+interface TodoCommandArgs {
+  title: string;
+  description: string;
+}
+
+export default class TodoAddCommand extends Command {
+  constructor(client: CommandoClient) {
+    super(client, {
+      name: 'todo_add',
+      group: 'paiza',
+      memberName: 'todo_add',
+      description: '忘れないで あの日の思い出',
+      args: [
+        {
+          key: 'title',
+          prompt: 'todo title (only [add])',
+          type: 'string',
+        },
+      ],
+    } as CommandInfo);
+  }
+
+  async run(
+    msg: CommandoMessage,
+    args: TodoCommandArgs,
+  ): Promise<CommandoMessage> {
+    const userId = msg.author.id;
+
+    const todo = new Todo();
+    todo.userId = userId;
+    todo.title = args.title;
+
+    await todo.save();
+
+    return msg.say(`success. [${todo.title}]`);
+  }
+}
